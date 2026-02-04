b2e897d25476c8dec50114d983e554603878707f
danial <trody2001@gmail.com>
2026-02-04 17:46:02 +0200

    Complete temperature_converter.py

---
+++ 02_Primitive_Types/class_exercises/temperature_converter.py
@@ -0,0 +1,7 @@
+celsius: float = float(input("Please enter a temperature in Celsius: "))
+fahrenheit: float = (celsius * 9 / 5) + 32
+kelvin: float = celsius + 273.15
+
+print(f"Temperature in Celsius: {celsius}")
+print(f"Temperature in Fahrenheit: {fahrenheit}")
+print(f"Temperature in Kelvin: {kelvin}")
