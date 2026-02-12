9c3b95a26659ad81d3c8d8f8c68254ee00fb414c
danial <trody2001@gmail.com>
2026-02-12 23:06:49 +0200

    HW2-ex1: solved WPS202 warning

--- homework/solutions/hw2/tests/test_exercise1.py
+++ homework/solutions/hw2/tests/test_exercise1.py
@@ -21,7 +21,8 @@ def test_long_log_content():
     log_content = """
 2024-04-29 15:45:00,089 INFO [name:starwars_engine][pid:2995] Message one
 2024-04-29 15:45:05,123 WARNING [name:starwars_engine][pid:2996] Check disk space
-2024-04-29 15:45:08,111 /var/log/apache2/server.access.log 172.18.0.12 - - "POST /api/command/?201dfd68-e48d-587b-e715-3ff83ef3af19 HTTP/1.1" 200
+2024-04-29 15:45:08,111 /var/log/apache2/server.access.log 172.18.0.12 - -
+"POST /api/command/?201dfd68-e48d-587b-e715-3ff83ef3af19 HTTP/1.1" 200
 2024-04-29 15:45:10,456 ERROR [name:starwars_engine][pid:2997] Failed to start engine
 2024-04-29 15:46:00,789 INFO [name:starwars_engine][pid:2998] All systems go
 """
