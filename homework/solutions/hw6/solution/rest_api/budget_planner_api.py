88c80c34a5273a9c4c6aed6c7240b2bf890ae18c
danial <trody2001@gmail.com>
2026-02-17 22:22:51 +0200

    HW6: Complete part 1

--- homework/solutions/hw6/solution/rest_api/budget_planner_api.py
+++ homework/solutions/hw6/solution/rest_api/budget_planner_api.py
@@ -10,6 +10,8 @@ from solution.business_logic.budget import Budget
 from solution.business_logic.expense import Expense
 from solution.business_logic.income import Income
 
+MESSAGE = "message"
+
 app = FastAPI()
 
 budget = Budget()
@@ -24,31 +26,31 @@ def get_all_income() -> list[Income]:
 def add_income(income: Income) -> dict:
     try:
         budget.add_income(income)
-        return {
-            "message": f"Income '{income.description}' of ${income.amount} added successfully."
-        }
     except ValueError as error:
-        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=str(error))
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=error)
+    return {
+        MESSAGE: f"Income '{income.description}' of ${income.amount} added successfully."
+    }
 
 
 @app.delete("/income/index/{index}", status_code=HTTP_200_OK)
 def delete_income_by_index(index: int) -> dict:
     try:
         budget.remove_income(index)
-        return {"message": f"Income '{index}' removed successfully."}
     except IndexError as error:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(error))
     except ValueError as error:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(error))
+    return {MESSAGE: f"Income '{index}' removed successfully."}
 
 
 @app.delete("/income/description/{description}", status_code=HTTP_200_OK)
 def delete_income_by_description(description: str) -> dict:
     try:
         budget.remove_income(description)
-        return {"message": f"Income '{description}' removed successfully."}
     except ValueError as error:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(error))
+    return {MESSAGE: f"Income '{description}' removed successfully."}
 
 
 @app.get("/expense", status_code=HTTP_200_OK)
@@ -60,31 +62,31 @@ def get_all_expense() -> list[Expense]:
 def add_expense(expense: Expense) -> dict:
     try:
         budget.add_expense(expense)
-        return {
-            "message": f"Expense '{expense.description}' of ${expense.amount} added successfully."
-        }
     except ValueError as error:
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=str(error))
+    return {
+        MESSAGE: f"Expense '{expense.description}' of ${expense.amount} added successfully."
+    }
 
 
 @app.delete("/expense/index/{index}", status_code=HTTP_200_OK)
 def delete_expense_by_index(index: int) -> dict:
     try:
         budget.remove_expense(index)
-        return {"message": f"Expense '{index}' removed successfully."}
     except IndexError as error:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(error))
     except ValueError as error:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(error))
+    return {MESSAGE: f"Expense '{index}' removed successfully."}
 
 
 @app.delete("/expense/description/{description}", status_code=HTTP_200_OK)
 def delete_expense_by_description(description: str) -> dict:
     try:
         budget.remove_expense(description)
-        return {"message": f"Expense '{description}' removed successfully."}
     except ValueError as error:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(error))
+    return {MESSAGE: f"Expense '{description}' removed successfully."}
 
 
 @app.get("/summary", response_class=PlainTextResponse, status_code=HTTP_200_OK)
@@ -95,4 +97,4 @@ def get_summary() -> str:
 @app.delete("/clear", status_code=HTTP_200_OK)
 def clear_all_data() -> dict:
     budget.clear_all()
-    return {"message": "Budget Cleared successfully!"}
+    return {MESSAGE: "Budget Cleared successfully!"}
