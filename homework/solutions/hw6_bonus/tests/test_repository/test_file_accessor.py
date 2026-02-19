15c24a353b8c0b85e74a3b002b638ec8391450df
danial <trody2001@gmail.com>
2026-02-19 22:56:14 +0200

    HW6_BONUS: test_file_accessor.py

--- homework/solutions/hw6_bonus/tests/test_repository/test_file_accessor.py
+++ homework/solutions/hw6_bonus/tests/test_repository/test_file_accessor.py
@@ -1,13 +1,63 @@
+import json
+import pytest
 from pathlib import Path
 from solution.repository.file_accessor import JsonFileAccessor
 
+TEST_FILE_NAME = "test_data.json"
+WRONG_FILE_NAME = "not_exists.json"
+NEW_FILE_JSON = "new_file.json"
 
-def test_write_and_read(tmp_path: Path) -> None:
-    file_path = tmp_path / "test.json"
+
+def json_content() -> dict[str, object]:
+    return {"key": "value", "number": 42}
+
+
+@pytest.fixture
+def prepared_json_file(tmp_path: Path) -> Path:
+    """Fixture to prepare a JSON file for testing."""
+    file_path = tmp_path / TEST_FILE_NAME
+    with open(file_path, "w") as file:
+        json.dump(json_content(), file)
+    return file_path
+
+
+def test_read(prepared_json_file: Path) -> None:
+    file_path = str(prepared_json_file)
+    accessor = JsonFileAccessor(file_path)
+    content = accessor.read()
+    assert content == json_content()
+
+
+def test_write(prepared_json_file: Path) -> None:
+    file_path = str(prepared_json_file)
+    accessor = JsonFileAccessor(file_path)
+    new_content = {"new": "data", "num": 123}
+
+    accessor.write(new_content)
+
+    with open(file_path, "r") as file:
+        content = json.load(file)
+
+    assert content == new_content
+
+
+def test_read_non_existing_file(tmp_path: Path) -> None:
+    file_path = tmp_path / WRONG_FILE_NAME
     accessor = JsonFileAccessor(str(file_path))
 
-    data = {"name": "Danial", "age": 25}
-    accessor.write(data)
+    content = accessor.read()
 
-    read_data = accessor.read()
-    assert read_data == data
+    assert content == {}
+
+
+def test_write_non_existing_file(tmp_path: Path) -> None:
+    file_path = tmp_path / NEW_FILE_JSON
+    accessor = JsonFileAccessor(str(file_path))
+    new_content = {"new": "data", "num": 123}
+
+    accessor.write(new_content)
+
+    with open(file_path, "r") as file:
+        content = json.load(file)
+
+    assert content == new_content
