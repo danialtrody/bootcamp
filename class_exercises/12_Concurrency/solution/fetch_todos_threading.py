90facb965217d44a80fb73be1aba97d1229f8cc9
danial <trody2001@gmail.com>
2026-02-22 13:04:55 +0200

    calss-exe: complete 12_exercise (1-2)

--- class_exercises/12_Concurrency/solution/fetch_todos_threading.py
+++ class_exercises/12_Concurrency/solution/fetch_todos_threading.py
@@ -1,39 +1,51 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 from typing import Any
+
 import requests
 
 MAX_WORKERS = 8
+REQUESTS = 20
+
 
 def fetch_todo(todo_id: int) -> dict[str, Any]:
     try:
         response = requests.get(
             f"https://jsonplaceholder.typicode.com/todos/{todo_id}",
+            timeout=10,
         )
-        response.raise_for_status()
-
-        data = response.json()
-        print(f"TODO {data['id']}: {data['title']}")
-
-        return data
-
-    except requests.exceptions.RequestException as e:
-        print(f"Failed fetching TODO {todo_id}: {e}")
+    except requests.exceptions.RequestException:
         return {}
+
+    return response.json()
+
+
+def run_requests() -> float:
+    start_time = time.time()
+
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        futures = [
+            executor.submit(fetch_todo, todo_id)
+            for todo_id in range(1, REQUESTS + 1)
+        ]
+
+        for future in as_completed(futures):
+            todo = future.result()
+            print(f"TODO {todo['id']}: {todo['title']}")
+
+    return time.time() - start_time
+
+
+def main() -> None:
+    print("Fetching 20 TODO items using ThreadPoolExecutor...")
+
+    execution_duration = run_requests()
+    avg_time = execution_duration / REQUESTS
+
+    print("Summary:")
+    print(f"Total execution time: {execution_duration:.2f} seconds")
+    print(f"Average time per request: {avg_time:.2f} seconds")
 
 
 if __name__ == "__main__":
-    start_execution_time = time.time()
-
-    print("Fetching 20 TODO items using ThreadPoolExecutor...")
-    
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        futures = [executor.submit(fetch_todo,id) for id in range(1,21)]
-        for future in as_completed(futures):
-            future.result()
-            
-    print("Summary:")
-    execution_duration = time.time() - start_execution_time
-    avg_per_request = execution_duration/20
-    print(f"Total execution time: {execution_duration:.2f} seconds")
-    print(f"Average time per request: {avg_per_request:.2f} seconds")
+    main()
