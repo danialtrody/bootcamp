c4730629c4573074c75277421d2cb1d42e71aaf1
danial <trody2001@gmail.com>
2026-03-08 01:00:41 +0200

    Complete part_2 Layer 3 (FastAPI Layer) reports_router

--- homework/solutions/hw8/solution/api/routers/reports_router.py
+++ homework/solutions/hw8/solution/api/routers/reports_router.py
@@ -1,15 +1,11 @@
-from re import S
-
 from fastapi import APIRouter, Depends, HTTPException
 from solution.services.report_service import ReportService
 from solution.api.dependencies import get_report_service
-from typing import List, Dict, Optional
+from typing import Optional
 from decimal import Decimal
 
 from starlette.status import (
     HTTP_400_BAD_REQUEST,
-    HTTP_201_CREATED,
-    HTTP_404_NOT_FOUND,
     HTTP_200_OK,
 )
 
@@ -19,32 +15,27 @@ reports_service_dependency: ReportService = Depends(get_report_service)
 
 @router.get("/monthly-summary", status_code=HTTP_200_OK)
 def get_monthly_summary(
-        month: int,
-        year: int,
-        account_id: Optional[int] = None,
-        service: ReportService = reports_service_dependency
-    ) -> dict[str, Decimal]:
-    
+    month: int,
+    year: int,
+    account_id: Optional[int] = None,
+    service: ReportService = reports_service_dependency,
+) -> dict[str, Decimal]:
+
     try:
         return service.get_monthly_summary(month, year, account_id)
     except ValueError as error:
-        raise HTTPException(
-            status_code=HTTP_400_BAD_REQUEST,
-            detail= str(error)
-        )
-        
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=str(error))
+
+
 @router.get("/spending-breakdown", status_code=HTTP_200_OK)
 def get_spending_breakdown_by_category(
-        month: int,
-        year: int,
-        account_id: Optional[int] = None,
-        service: ReportService = reports_service_dependency
-    ) -> dict[str, Decimal]:
-    
+    month: int,
+    year: int,
+    account_id: Optional[int] = None,
+    service: ReportService = reports_service_dependency,
+) -> dict[str, Decimal]:
+
     try:
         return service.get_spending_breakdown_by_category(month, year, account_id)
     except ValueError as error:
-        raise HTTPException(
-            status_code=HTTP_400_BAD_REQUEST,
-            detail= str(error)
-        )
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=str(error))
