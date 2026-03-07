f00ff3269705f8f2b056602d5fc0d43cf5d1283e
danial <trody2001@gmail.com>
2026-03-07 23:02:17 +0200

    Complete part_2 Layer 3 (FastAPI Layer) net_worth_router

--- homework/solutions/hw8/solution/api/routers/net_worth_router.py
+++ homework/solutions/hw8/solution/api/routers/net_worth_router.py
@@ -1,20 +1,17 @@
+from decimal import Decimal
 from fastapi import APIRouter, Depends, HTTPException
-
-
 from solution.services.net_worth_service import NetWorth
 from solution.api.dependencies import get_net_worth_service
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 
-router = APIRouter(prefix="/net_worth", tags=["Net Worth"])
+router = APIRouter(prefix="/net-worth", tags=["Net Worth"])
+net_worth_service_dependency: NetWorth = Depends(get_net_worth_service)
 
 
+@router.get("/", status_code=HTTP_200_OK)
+def get_all_account_net_worth(service: NetWorth = net_worth_service_dependency) -> Decimal:
 
-@router.get("/")
-def get_all_account_net_worth(
-    service: NetWorth = Depends(get_net_worth_service)
-    ):
-        
     try:
         return service.calculate_net_worth()
-    except Exception  as error:
-        raise HTTPException(status_code=400, detail=str(error))
-    
+    except ValueError as error:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=str(error))
