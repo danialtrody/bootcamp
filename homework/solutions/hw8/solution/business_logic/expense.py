4e25dbbdf9b75ec0bd6d31998e4242f6c9fa7f23
danial <trody2001@gmail.com>
2026-03-10 00:21:10 +0200

    Complete part_2 Layer 4 (TUI) transaction_cli

--- homework/solutions/hw8/solution/business_logic/expense.py
+++
@@ -1,9 +0,0 @@
-from solution.business_logic.transaction import Transaction
-
-
-class Expense(Transaction):
-    """
-    Represents an expense entry.
-
-    Inherits all attributes and validation from Transaction.
-    """
--- homework/solutions/hw8/solution/business_logic/income.py
+++
@@ -1,9 +0,0 @@
-from solution.business_logic.transaction import Transaction
-
-
-class Income(Transaction):
-    """
-    Represents an income entry.
-
-    Inherits all attributes and validation from Transaction.
-    """
