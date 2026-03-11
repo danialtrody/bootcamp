76dfe12d20073a2814f039f4ab24d82335af49e0
danial <trody2001@gmail.com>
2026-03-11 19:47:15 +0200

    fix flake8

--- homework/solutions/hw9/solution/database.py
+++ homework/solutions/hw9/solution/database.py
@@ -15,5 +15,6 @@ SQLALCHEMY_DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
+
 class Base(DeclarativeBase):
-    pass
+    """"""
