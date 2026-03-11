3cc1fc9ffb1a5392de11b7d464c574dfcdc7308e
danial <trody2001@gmail.com>
2026-03-11 19:14:56 +0200

    add all required methods

--- homework/solutions/hw9/solution/database.py
+++ homework/solutions/hw9/solution/database.py
@@ -1,4 +1,19 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}"
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(DeclarativeBase):
     pass
